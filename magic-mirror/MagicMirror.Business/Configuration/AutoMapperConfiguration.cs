5b6cf0d6dffe5714b6662538011ff07467a73842
Michiel Wouters <Michiel.wouters.biz@gmail.com>
2017-07-22 17:16:29 +0200

    Added mapper information for WeatherModel

---
+++ magic-mirror/MagicMirror.Business/Configuration/AutoMapperConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MagicMirror.Business.Models.Traffic;
+using MagicMirror.Business.Models.Weather;
+using MagicMirror.DataAccess.Entities.Weather;
+using MagicMirror.Entities.Traffic;
+
+namespace MagicMirror.Business.Configuration
+{
+    public class AutoMapperConfiguration : Profile
+    {
+        public AutoMapperConfiguration()
+        {
+            CreateMap<WeatherEntity, WeatherModel>()
+                    .ForMember(dest => dest.SunRise, source => source.MapFrom(src => src.Sys.Sunrise))
+                    .ForMember(dest => dest.SunSet, source => source.MapFrom(src => src.Sys.Sunset))
+                    .ForMember(dest => dest.WeatherType, source => source.MapFrom(src => src.Weather[0].Main))
+                    .ForMember(dest => dest.Icon, source => source.MapFrom(src => src.Weather[0].Icon))
+                    .ForMember(dest => dest.Description, source => source.MapFrom(src => src.Weather[0].Description))
+                .ReverseMap();
+
+            CreateMap<TrafficEntity, TrafficModel>()
+                .ReverseMap();
+        }
+    }
+}
