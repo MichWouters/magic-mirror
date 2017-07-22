531056d93ed1b62871cb74e789fb264ff3eb1f46
Michiel Wouters <Michiel.wouters.biz@gmail.com>
2017-07-22 17:07:51 +0200

    Added Business Models

---
+++ magic-mirror/MagicMirror.Business.Models/Traffic/TrafficModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MagicMirror.Business.Models.Traffic
+{
+    public class TrafficModel
+    {
+        public string Distance { get; set; }
+
+        public string Minutes { get; set; }
+
+        public int NumberOfIncidents { get; set; }
+
+        public DateTime HourOfArrival { get; set; }
+
+        public TrafficDensity TrafficDensity { get; set; }
+    }
+
+    public enum TrafficDensity
+    {
+        Few, Light, Medium, Heavy
+    }
+}
---
+++ magic-mirror/MagicMirror.Business.Models/Weather/WeatherModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MagicMirror.Business.Models.Weather
+{
+    public class WeatherModel
+    {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+        public double DegreesCelsius { get; set; }
+        public string WeatherType { get; set; }
+
+        public string SunRise { get; set; }
+        public string SunSet { get; set; }
+    }
+}
