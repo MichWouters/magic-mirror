fea305594f6204e7de2737b208d917e0743e15c3
Michiel Wouters <Michiel.wouters.biz@gmail.com>
2017-07-29 23:12:33 +0200

    Added frontend project

--- magic-mirror/MagicMirror.Business/Services/Traffic/TrafficService.cs
+++ magic-mirror/MagicMirror.Business/Services/Traffic/TrafficService.cs
@@ -1,11 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using MagicMirror.Business.Models.Traffic;
+using MagicMirror.DataAccess.Entities;
 
 namespace MagicMirror.Business.Services.Traffic
 {
-    public class TrafficService: ServiceBase
+    public class TrafficService : ServiceBase, IService<TrafficModel>
     {
+        public TrafficModel CalculateMappedValues(TrafficModel model)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<TrafficModel> GetModelAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TrafficModel MapEntityToModel(Entity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
