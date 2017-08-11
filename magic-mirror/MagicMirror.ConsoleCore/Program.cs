3f844e5f668251c64d27190073600a2cea4cb7ab
Michiel Wouters <Michiel.wouters.biz@gmail.com>
2017-08-11 22:58:19 +0200

    Added a simple console application to showcase the communication between layers

--- magic-mirror/MagicMirror.ConsoleCore/Program.cs
+++ magic-mirror/MagicMirror.ConsoleCore/Program.cs
@@ -8,14 +8,17 @@ namespace MagicMirror.ConsoleCore
 {
     public class Program
     {
-        private static IService<WeatherModel> _service;
+        private static IService<WeatherModel> _weatherService;
+        private static IService<TrafficModel> _trafficService;
 
         public static void Main(string[] args)
         {
             SearchCriteria criteria = GatherUserInformation();
             Console.WriteLine("Crunching the numbers...");
+            Console.WriteLine();
 
-            Task.Run(() => GenerateInformation(criteria));
+            MagicMirrorDto dto = Task.Run(() => GenerateDto(criteria)).Result;
+            OutputData(dto);
             Console.ReadLine();
         }
 
@@ -25,8 +28,11 @@ namespace MagicMirror.ConsoleCore
             Console.WriteLine("Please enter your name: ");
             string user = Console.ReadLine();
 
+            Console.WriteLine("Please enter your street and house number: ");
+            string address = Console.ReadLine();
+
             Console.WriteLine("Please enter your home town: ");
-            string homeAddress = Console.ReadLine();
+            string city = Console.ReadLine();
 
             Console.WriteLine("Please enter your work address: ");
             string workAddress = Console.ReadLine();
@@ -34,20 +40,47 @@ namespace MagicMirror.ConsoleCore
             var criteria = new SearchCriteria
             {
                 UserName = user,
-                City = homeAddress,
-                Destination = workAddress
+                Start = $"{address}, {city}",
+                City = city,
+                Destination = workAddress,
             };
 
             return criteria;
         }
 
-        private static async Task<WeatherModel> GenerateInformation(SearchCriteria criteria)
+        private static async Task<MagicMirrorDto> GenerateDto(SearchCriteria criteria)
         {
-            _service = new WeatherService();
-            WeatherModel result = await _service.GetModelAsync(criteria);
+            try
+            {
+                _weatherService = new WeatherService();
+                _trafficService = new TrafficService();
+                WeatherModel weatherModel = await _weatherService.GetModelAsync(criteria);
+                TrafficModel trafficModel = await _trafficService.GetModelAsync(criteria);
 
-            Console.WriteLine($"Welcome {criteria.UserName} The current top-side temperature is {result.TemperatureCelsius} degrees Celsius");
-            return result;
+                var dto = new MagicMirrorDto
+                {
+                    UserName = criteria.UserName,
+                    DegreesCelsius = weatherModel.TemperatureCelsius,
+                    TravelTime = trafficModel.MinutesText,
+                    TrafficDensity = trafficModel.TrafficDensity,
+                    WeatherType = weatherModel.WeatherType
+                };
+
+                return dto;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private static void OutputData(MagicMirrorDto dto)
+        {
+            Console.WriteLine($"Hello {dto.UserName}");
+            Console.WriteLine($"Today is {DateTime.Now:D}. The current time is: {DateTime.Now:t}");
+            Console.WriteLine($"The current top-side temperature is {dto.DegreesCelsius} degrees Celsius with {dto.WeatherType}");
+            Console.WriteLine($"Your travel time is {dto.TravelTime} with {dto.TrafficDensity} traffic.");
         }
     }
 }
