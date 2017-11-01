ddaddfde10cc1ad8e6fb9f22b7a9f399cefa0446
Michiel Wouters <michiel_woute64@hotmail.com>
2017-11-01 23:24:54 +0100

    extra commands

--- magic-mirror/MagicMirror.Business.Models/Weather/WeatherModel.cs
+++ magic-mirror/MagicMirror.Business.Models/Weather/WeatherModel.cs
@@ -3,25 +3,15 @@
     public class WeatherModel : IModel
     {
         public string Location { get; set; }
-
         public string Icon { get; set; }
-
         public double TemperatureCelsius { get; set; }
-
         public double TemperatureKelvin { get; set; }
-
         public double TemperatureFahrenheit { get; set; }
-
         public double Temperature { get; set; }
-
         public string WeatherType { get; set; }
-
         public string SunRise { get; set; }
-
         public int SunRiseMilliseconds { get; set; }
-
         public string SunSet { get; set; }
-
         public int SunSetMilliSeconds { get; set; }
     }
 }
